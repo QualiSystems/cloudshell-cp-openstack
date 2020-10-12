baf2d83b575a9e824f99767191fe84aa60b6e954
Kyrylo Maksymenko <saklar13@gmail.com>
2020-10-12 19:14:24 +0300

    new tests for deploy app

--- tests/cloudshell/cp/openstack/models/test_deploy_app.py
+++ tests/cloudshell/cp/openstack/models/test_deploy_app.py
@@ -1,6 +1,12 @@
+import pytest
+
 from cloudshell.cp.core.request_actions import DeployVMRequestActions
 
 from cloudshell.cp.openstack.models import OSNovaImgDeployApp
+from cloudshell.cp.openstack.models.deploy_app import (
+    ResourceInboundPortsRO,
+    SecurityGroupRule,
+)
 
 
 def test_deploy_app(deploy_app_request_factory, cs_api):
@@ -41,3 +47,48 @@ def test_deploy_app(deploy_app_request_factory, cs_api):
     assert app.password == password
     assert app.public_ip == public_ip
     assert app.actionId == action_id
+
+
+@pytest.mark.parametrize(
+    ("string", "cidr", "protocol", "port_min", "port_max"),
+    (
+        ("192.168.1.0/24:udp:4500", "192.168.1.0/24", "udp", 4500, 4500),
+        ("5000-5100", "0.0.0.0/0", "tcp", 5000, 5100),
+        ("10.0.1.0/24:22", "10.0.1.0/24", "tcp", 22, 22),
+        ("udp:450", "0.0.0.0/0", "udp", 450, 450),
+    ),
+)
+def test_security_group_rule(string, cidr, protocol, port_min, port_max):
+    sgr = SecurityGroupRule.from_str(string)
+
+    assert sgr.cidr == cidr
+    assert sgr.protocol == protocol
+    assert sgr.port_range_min == port_min
+    assert sgr.port_range_max == port_max
+
+
+@pytest.mark.parametrize("string", ("", "incorrect_cidr:udp:450"))
+def test_security_group_incorrect_string(string):
+    with pytest.raises(ValueError, match="Security group rule is not supported format"):
+        SecurityGroupRule.from_str(string)
+
+
+def test_resource_inbound_ports_ro():
+    class RConfig:
+        DEPLOYMENT_PATH = "DP"
+        attributes = {"DP.Inbound Ports": "22;udp:4500"}
+        inbound_ports = ResourceInboundPortsRO("Inbound Ports")
+
+    assert type(RConfig.inbound_ports) == ResourceInboundPortsRO
+    rconf = RConfig()
+    assert len(rconf.inbound_ports) == 2
+    port1 = rconf.inbound_ports[0]
+    assert port1.cidr == "0.0.0.0/0"
+    assert port1.protocol == "tcp"
+    assert port1.port_range_min == 22
+    assert port1.port_range_max == 22
+    port2 = rconf.inbound_ports[1]
+    assert port2.cidr == "0.0.0.0/0"
+    assert port2.protocol == "udp"
+    assert port2.port_range_min == 4500
+    assert port2.port_range_max == 4500
