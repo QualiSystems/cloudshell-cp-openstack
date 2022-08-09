b374a05f9a8a1d74c6920abc79d3bf85b6df9faa
Kyrylo Maksymenko <saklar13@gmail.com>
2022-08-09 18:04:23 +0200

    creating new API

--- cloudshell/cp/openstack/os_api/models/port.py
+++ cloudshell/cp/openstack/os_api/models/port.py
@@ -1,29 +1,40 @@
 from __future__ import annotations
 
+from contextlib import suppress
+from logging import Logger
 from threading import Lock
+from typing import TYPE_CHECKING, ClassVar
 
 import attr
 from neutronclient.common import exceptions as neutron_exc
 from neutronclient.v2_0.client import Client as NeutronClient
 
 from cloudshell.cp.openstack.exceptions import PortNotFound
-from cloudshell.cp.openstack.os_api.os_api_models.network import Network
+from cloudshell.cp.openstack.os_api.models.network import Network
 from cloudshell.cp.openstack.utils.cached_property import cached_property
 
+if TYPE_CHECKING:
+    from cloudshell.cp.openstack.api.api import OsApi
 
-@attr.s(auto_attribs=True)
+
+@attr.s(auto_attribs=True, str=False)
 class Port:
     LOCK = Lock()
-    _neutron: NeutronClient = attr.ib(repr=False)
+    api: ClassVar[OsApi]
+    _neutron: ClassVar[NeutronClient]
+    _logger: ClassVar[Logger]
+
     id: str  # noqa: A003
     name: str
     network_id: str
     mac_address: str
 
+    def __str__(self) -> str:
+        return f"Port '{self.name}'"
+
     @classmethod
-    def from_dict(cls, neutron: NeutronClient, port_dict: dict) -> Port:
+    def from_dict(cls, port_dict: dict) -> Port:
         return cls(
-            neutron,
             port_dict["id"],
             port_dict["name"],
             port_dict["network_id"],
@@ -31,52 +42,55 @@ class Port:
         )
 
     @classmethod
-    def get(cls, neutron: NeutronClient, id_: str) -> Port:
+    def get(cls, id_: str) -> Port:
+        cls._logger.debug(f"Getting a port with ID '{id_}'")
         try:
-            port_dict = neutron.show_port(id_)["port"]
+            port_dict = cls._neutron.show_port(id_)["port"]
         except neutron_exc.PortNotFoundClient:
             raise PortNotFound(id_=id_)
-        return cls.from_dict(neutron, port_dict)
+        return cls.from_dict(port_dict)
 
     @classmethod
-    def find(cls, neutron: NeutronClient, name: str) -> Port:
-        for port_dict in neutron.list_ports(name=name)["ports"]:
+    def find_first(cls, name: str) -> Port:
+        cls._logger.debug(f"Searching for first port with name '{name}'")
+        for port_dict in cls._neutron.list_ports(name=name)["ports"]:
             if port_dict["name"] == name:
                 break
         else:
             raise PortNotFound(name=name)
-        return cls.from_dict(neutron, port_dict)
+        return cls.from_dict(port_dict)
 
     @classmethod
     def create(
         cls,
-        neutron: NeutronClient,
         name: str,
         network: Network,
         mac_address: str | None = None,
     ) -> Port:
         port_data = {"name": name, "network_id": network.id, "mac_address": mac_address}
-        full_port_dict = neutron.create_port({"port": port_data})["port"]
-        return cls.from_dict(neutron, full_port_dict)
+        cls._logger.debug(f"Creating a port with data {port_data}")
+        full_port_dict = cls._neutron.create_port({"port": port_data})["port"]
+        return cls.from_dict(full_port_dict)
 
     @classmethod
     def find_or_create(
         cls,
-        neutron: NeutronClient,
         name: str,
         network: Network,
         mac: str | None = None,
     ) -> Port:
         with cls.LOCK:
             try:
-                port = cls.find(neutron, name)
+                port = cls.find_first(name)
             except PortNotFound:
-                port = cls.create(neutron, name, network, mac)
+                port = cls.create(name, network, mac)
         return port
 
     @cached_property
     def network(self) -> Network:
-        return Network.get(self._neutron, self.network_id)
+        return self.api.Network.get(self.network_id)
 
     def remove(self) -> None:
-        self._neutron.delete_port(self.id)
+        self._logger.debug(f"Removing the {self}")
+        with suppress(neutron_exc.PortNotFoundClient):
+            self._neutron.delete_port(self.id)
