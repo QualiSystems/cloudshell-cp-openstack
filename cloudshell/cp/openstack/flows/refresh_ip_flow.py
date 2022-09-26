76b58554a0af4ab37a31e68eba246c65d8e3a39f
Kyrylo Maksymenko <saklar13@gmail.com>
2022-09-26 19:25:34 +0200

    update refresh ip flow

--- cloudshell/cp/openstack/flows/refresh_ip_flow.py
+++ cloudshell/cp/openstack/flows/refresh_ip_flow.py
@@ -1,23 +1,18 @@
+from cloudshell.cp.openstack.api.api import OsApi
 from cloudshell.cp.openstack.models import OSNovaImgDeployedApp
-from cloudshell.cp.openstack.os_api.api import OSApi
 from cloudshell.cp.openstack.resource_config import OSResourceConfig
-from cloudshell.cp.openstack.utils.instance_helpers import (
-    get_floating_ip,
-    get_private_ip,
-)
 
 
 def refresh_ip(
-    api: OSApi,
+    api: OsApi,
     deployed_app: OSNovaImgDeployedApp,
     resource_conf: OSResourceConfig,
 ):
-    instance = api.get_instance(deployed_app.vmdetails.uid)
+    instance = api.Instance.get(deployed_app.vmdetails.uid)
+    mgmt_iface = instance.find_interface_by_port_name("mgmt")
 
-    private_net_name = api.get_network_name(resource_conf.os_mgmt_net_id)
-    new_private_ip = get_private_ip(instance, private_net_name)
-
-    new_public_ip = get_floating_ip(instance)
+    new_private_ip = mgmt_iface.fixed_ip
+    new_public_ip = mgmt_iface.floating_ip
 
     if new_private_ip != deployed_app.private_ip:
         deployed_app.update_private_ip(deployed_app.name, new_private_ip)
