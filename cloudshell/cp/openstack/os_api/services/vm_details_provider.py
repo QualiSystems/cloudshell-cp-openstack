94421b9e9649590b39dcb169650f98294e2e5dd4
Kyrylo Maksymenko <saklar13@gmail.com>
2022-09-26 19:19:53 +0200

    update creating VM details

--- cloudshell/cp/openstack/os_api/services/vm_details_provider.py
+++ cloudshell/cp/openstack/os_api/services/vm_details_provider.py
@@ -1,6 +1,4 @@
-from typing import List
-
-from novaclient.v2.servers import Server as NovaServer
+from __future__ import annotations
 
 from cloudshell.cp.core.request_actions.models import (
     VmDetailsData,
@@ -8,60 +6,49 @@ from cloudshell.cp.core.request_actions.models import (
     VmDetailsProperty,
 )
 
-from cloudshell.cp.openstack.os_api.api import OSApi
-from cloudshell.cp.openstack.utils.instance_helpers import (
-    get_floating_ip,
-    get_private_ip,
-)
+from cloudshell.cp.openstack.os_api.models import Instance
 
 
-def create(instance: NovaServer, api: OSApi, management_vlan_id) -> VmDetailsData:
-    instance.get()  # update instance data
-    vm_instance = _get_vm_instance_data(instance, api)
-    vm_network = _get_vm_network_data(instance, api, management_vlan_id)
+def create(instance: Instance, management_net_id: str) -> VmDetailsData:
+    vm_instance = _get_vm_instance_data(instance)
+    vm_network = _get_vm_network_data(instance, management_net_id)
     return VmDetailsData(
         vmInstanceData=vm_instance, vmNetworkData=vm_network, appName=instance.name
     )
 
 
-def _get_vm_instance_data(instance: NovaServer, api: OSApi) -> List[VmDetailsProperty]:
-    image = api.get_image_from_instance(instance)
-    flavor = api.get_flavor_from_instance(instance)
-    available_zone = getattr(instance, "OS-EXT-AZ:availability_zone")
+def _get_vm_instance_data(instance: Instance) -> list[VmDetailsProperty]:
+    flavor = instance.flavor
     return [
-        VmDetailsProperty("Image", image.name),
+        VmDetailsProperty("Image", instance.image.name),
         VmDetailsProperty("Flavor", flavor.name),
-        VmDetailsProperty("Availability Zone", available_zone),
+        VmDetailsProperty("Availability Zone", instance.available_zone),
         VmDetailsProperty("CPU", f"{flavor.vcpus} vCPU"),
-        # OpenStack returns RAM in MB and disk in GB
-        VmDetailsProperty("Memory", f"{flavor.ram} MB"),
-        VmDetailsProperty("Disk Size", f"{flavor.disk} GB"),
+        VmDetailsProperty("Memory", f"{flavor.ram_mb} MB"),
+        VmDetailsProperty("Disk Size", f"{flavor.disk_gb} GB"),
     ]
 
 
 def _get_vm_network_data(
-    instance: NovaServer, api: OSApi, mgmt_net_id
-) -> List[VmDetailsNetworkInterface]:
+    instance: Instance, mgmt_net_id: str
+) -> list[VmDetailsNetworkInterface]:
     network_interfaces = []
-    for interface in instance.interface_list():
-        net_dict = api.get_network_dict(id=interface.net_id)
-        net_name = net_dict["name"]
-        private_ip = get_private_ip(instance, net_name)
-        is_primary_and_predefined = mgmt_net_id == interface.net_id
-        # todo why we add it to every interface and check for the whole instance??
-        public_ip = get_floating_ip(instance)
+    for iface in instance.interfaces:
+        private_ip = iface.fixed_ip
+        public_ip = iface.floating_ip
+        is_primary_and_predefined = mgmt_net_id == iface.network_id
 
         network_data = [
             VmDetailsProperty("IP", private_ip),
-            VmDetailsProperty("MAC Address", interface.mac_addr),
-            VmDetailsProperty("VLAN Name", net_name, hidden=True),
+            VmDetailsProperty("MAC Address", iface.mac_address),
+            VmDetailsProperty("VLAN Name", iface.network.name, hidden=True),
         ]
         if public_ip:
             network_data.append(VmDetailsProperty("Floating IP", public_ip))
 
         current_interface = VmDetailsNetworkInterface(
-            interfaceId=interface.mac_addr,
-            networkId=interface.net_id,
+            interfaceId=iface.mac_address,
+            networkId=iface.network_id,
             isPrimary=is_primary_and_predefined,
             isPredefined=is_primary_and_predefined,
             networkData=network_data,
@@ -69,4 +56,12 @@ def _get_vm_network_data(
             publicIpAddress=public_ip,
         )
         network_interfaces.append(current_interface)
-    return sorted(network_interfaces, key=lambda x: x.networkId)
+    return sorted(network_interfaces, key=_sort_networks_key(mgmt_net_id))
+
+
+def _sort_networks_key(mgmt_net_id: str):
+    def sort(net_iface: VmDetailsNetworkInterface) -> bool:
+        # iface with mgmt network would be first
+        return net_iface.networkId != mgmt_net_id
+
+    return sort
