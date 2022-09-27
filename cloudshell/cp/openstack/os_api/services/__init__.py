65235c22fb1bdca9fe11a7fb4aa52f4c8860cdb1
Kyrylo Maksymenko <saklar13@gmail.com>
2022-09-27 18:19:20 +0200

    remove unused

--- cloudshell/cp/openstack/os_api/services/__init__.py
+++ cloudshell/cp/openstack/os_api/services/__init__.py
@@ -1,5 +1,3 @@
-from .neutron.neutron_network_service import NeutronService
-from .nova.nova_instance_service import NovaService
 from .validator import validate_conf_and_connection
 
-__all__ = ["NeutronService", "NovaService", "validate_conf_and_connection"]
+__all__ = ["validate_conf_and_connection"]
